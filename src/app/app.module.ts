37a953154f9e0a3fe8d37ce4e0a1914d813e2a41
Alexandre Caron <96966192+Rachmaninoof@users.noreply.github.com>
2024-02-14 10:00:58 -0500

    testing deployement things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 import { UserBlocksComponent } from './user-blocks/user-blocks.component';
@@ -19,7 +18,6 @@ import { TurnComponent } from './turn/turn.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [],
